26dcbd90c32d5e989aa7553122ddc83788a10d1e
Gabe Kahen <gabe@gabekahen.com>
2018-04-01 22:24:15 -0400

    Added test for write function

--- book_test.go
+++ book_test.go
@@ -1,16 +1,32 @@
 package main
 
 import (
-  "testing"
-  "encoding/json"
+	"encoding/json"
+	"testing"
+	"time"
 )
 
-func TestPrint (t *testing.T) {
-  var outBook Book
-  title := "My Book"
-  inBook := Book{Title: title}
-  json.Unmarshal(inBook.print(), &outBook)
-  if outBook.Title != title {
-    t.Fatalf("Book.Print(): Expected Title 'My Book', got '%v'", outBook.Title)
-  }
+func TestPrint(t *testing.T) {
+	var outBook Book
+	title := "My Book"
+	inBook := Book{Title: title}
+	json.Unmarshal(inBook.print(), &outBook)
+	if outBook.Title != title {
+		t.Fatalf("Book.Print(): Expected Title 'My Book', got '%v'", outBook.Title)
+	}
+}
+
+func TestWrite(t *testing.T) {
+	book := Book{
+		Title:       "My Book",
+		Author:      "Mr. Author",
+		Publisher:   "Mr. Publisher",
+		PublishDate: time.Now().Unix(),
+		Rating:      4,
+		Status:      0,
+	}
+	err := book.write()
+	if err != nil {
+		t.Fatal(err)
+	}
 }
