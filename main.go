b1ea21d6060efa93e2024cf2f36386ae1c62b273
Gabe Kahen <gabe@gabekahen.com>
2018-04-02 14:24:28 -0600

    removed query string parsing from main.go

--- main.go
+++ main.go
@@ -5,47 +5,10 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"strconv"
-	"time"
 )
 
 func libraryCreateHandler(w http.ResponseWriter, r *http.Request) {
 	book := Book{}
-
-	for key, value := range r.URL.Query() {
-		switch key {
-		case `Title`:
-			book.Title = value[0]
-		case `Author`:
-			book.Author = value[0]
-		case `Publisher`:
-			book.Publisher = value[0]
-		case `PublishDate`:
-			i, err := strconv.ParseInt(value[0], 10, 64)
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				log.Printf("Invalid UNIX date: %s\n", value[0])
-				return
-			}
-			book.PublishDate = time.Unix(i, 0).UTC()
-		case `Rating`:
-			i, err := strconv.ParseInt(value[0], 10, 0)
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				log.Printf("Invalid Rating: %s\n", value[0])
-				return
-			}
-			book.Rating = int(i)
-		case `Status`:
-			i, err := strconv.ParseInt(value[0], 10, 0)
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				log.Printf("Invalid Status: %s\n", value[0])
-				return
-			}
-			book.Status = int(i)
-		}
-	}
 
 	err := book.write()
 	if err != nil {
