4a5f5e45707abead6005d774ef5000af883fb2fe
Gabe Kahen <gabe@gabekahen.com>
2018-04-01 22:23:48 -0400

    UID is now a SHA1 hash

--- book.go
+++ book.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/base64"
 	"encoding/json"
 	"os"
 )
 
 var (
 	// LibraryPath - temp directory for book storage
-	LibraryPath = "/Users/gabekahen/Documents/library"
+	LibraryPath = "/Users/gabekahen/Documents/library/"
 )
 
 // Book structure
 type Book struct {
-	UID         uint64
+	UID         string
 	Title       string
 	Author      string
 	Publisher   string
@@ -27,10 +29,15 @@ func (book *Book) print() []byte {
 }
 
 func (book *Book) write() error {
-	file, err := os.Create(LibraryPath + book.Title)
+	hasher := sha1.New()
+	hasher.Write([]byte(book.Title + book.Author + book.Publisher))
+	uid := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+	book.UID = uid
+	file, err := os.Create(LibraryPath + uid)
 	if err != nil {
 		return err
 	}
+	defer file.Close()
 
 	_, err = file.Write(book.print())
 	if err != nil {
