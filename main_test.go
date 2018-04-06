a688367b75faefb571f0174089fd5b4ef50e71ef
Gabe Kahen <gabe@gabekahen.com>
2018-04-06 17:35:26 -0600

    Added tests for update handler

--- main_test.go
+++ main_test.go
@@ -9,6 +9,7 @@ import (
 	"net/http/httptest"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestMain(m *testing.M) {
@@ -124,14 +125,6 @@ func TestLibraryRead(t *testing.T) {
 			status, http.StatusOK)
 	}
 
-	// Check the response body is what we expect.
-	/* TODO Update test to unmarshal response and look for correct attributes
-	expected := `{"UID":"` + string(book.UID) + `","Title":"test read endpoint","Author":"","Publisher":"","PublishDate":"0001-01-01T00:00:00Z","Rating":0,"Status":0}`
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
-	}
-	*/
 	// cleanup test book
 	book.delete()
 
@@ -143,4 +136,100 @@ func TestLibraryRead(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusInternalServerError)
 	}
+}
+
+func TestLibraryUpdate(t *testing.T) {
+	baseBook := Book{
+		Title:       "TestLibraryUpdate",
+		Author:      "Mr. TestLibraryUpdate",
+		Publisher:   "TestLibraryUpdate Publishing Co",
+		PublishDate: time.Now(),
+		Rating:      3,
+		Status:      1,
+	}
+
+	handlerUpdate := http.HandlerFunc(libraryUpdateHandler)
+
+	// Try updating the status from 1 -> 22, and the rating from 3 -> 2
+	t.Run("Update Status & Rating", func(t *testing.T) {
+		book := baseBook
+
+		err := book.create()
+		if err != nil {
+			t.Error(err)
+		}
+
+		reqString := fmt.Sprintf(
+			"/update?UID=%d&Status=%d&Rating=%d",
+			book.UID,
+			22,
+			2,
+		)
+		updateReq, err := http.NewRequest("GET", reqString, nil)
+		if err != nil {
+			t.Error(err)
+		}
+
+		rr := httptest.NewRecorder()
+		handlerUpdate.ServeHTTP(rr, updateReq)
+
+		// Check the status code is what we expect.
+		if status := rr.Code; status != http.StatusOK {
+			t.Logf("%+v", string(rr.Body.Bytes()))
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusOK)
+		}
+
+		// Check if the book was updated
+		newBook := Book{UID: book.UID}
+		newBook.read()
+		if newBook.Status != 22 {
+			t.Errorf("Expected book status 22, got %d", newBook.Status)
+		}
+		if newBook.Rating != 2 {
+			t.Errorf("Expected book rating 2, got %d", newBook.Rating)
+		}
+		book.delete() // cleanup
+	})
+
+	// Update the rating from 3 -> 5. Status should remain 1
+	t.Run("Update Rating only", func(t *testing.T) {
+		book := baseBook
+
+		err := book.create()
+		if err != nil {
+			t.Error(err)
+		}
+
+		reqString := fmt.Sprintf(
+			"/update?UID=%d&Rating=%d",
+			book.UID,
+			5,
+		)
+		updateReq, err := http.NewRequest("GET", reqString, nil)
+		if err != nil {
+			t.Error(err)
+		}
+
+		rr := httptest.NewRecorder()
+		handlerUpdate.ServeHTTP(rr, updateReq)
+
+		// Check the status code is what we expect.
+		if status := rr.Code; status != http.StatusOK {
+			t.Logf("%+v", string(rr.Body.Bytes()))
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusOK)
+		}
+
+		// Check if the book was updated
+		newBook := Book{UID: book.UID}
+		newBook.read()
+		if newBook.Status != 1 {
+			t.Errorf("Expected book status 1, but got %d", newBook.Status)
+		}
+		if newBook.Rating != 5 {
+			t.Errorf("Expected book rating 5, got %d", newBook.Rating)
+		}
+		book.delete()
+	})
 }
